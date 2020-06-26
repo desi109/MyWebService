cfd2b4b37fdd1dd4e265751b3dd0848b11f0386c
Desislava Milusheva <deisi_deisi@abv.bg>
2020-06-26 15:37:15 +0300

    Add all files

---
+++ src/main/java/com/softwareag/mywebservice/MyWebService.java
@@ -0,0 +1,16 @@
+package com.softwareag.mywebservice;
+
+/**
+ * A simple test service which calculate the addition of two numbers.
+ */
+public class MyWebService {
+
+    /**
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public int add(int num1, int num2){
+        return num1 + num2;
+    }
+}
